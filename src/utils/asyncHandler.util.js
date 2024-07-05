e10c10e65df41f515d91f87777575a9c1499605f
Vishal <vjatia449@gmail.com>
2024-07-05 15:07:46 +0530

    added user controller and route, fixed error in asyncHandler

--- src/utils/asyncHandler.util.js
+++ src/utils/asyncHandler.util.js
@@ -1,6 +1,6 @@
 /* Approach 1: Promise */
 const asyncHandler = (requestHandler) => {
-  (req, res, next) => {
+  return (req, res, next) => {
     Promise
       .resolve(requestHandler(req, res, next))
       .catch((error) => {
